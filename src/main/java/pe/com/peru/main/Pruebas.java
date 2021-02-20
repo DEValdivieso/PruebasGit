bae76c941a9836da86976217d753f596cf1d3222
DEValdivieso <developerbackend3@farmaciasperuanas.pe>
2021-02-19 19:48:58 -0500

    segunda linea

--- src/main/java/pe/com/peru/main/Pruebas.java
+++ src/main/java/pe/com/peru/main/Pruebas.java
@@ -3,5 +3,6 @@ package pe.com.peru.main;
 public class Pruebas {
     public static void main(String args []){
         System.out.println("Hola mundo");
+        System.out.println("nueva linea");
     }
 }
